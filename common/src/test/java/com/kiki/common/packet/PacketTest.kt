31e6e365eb26d1d9233cc24f75c9bc46043c0cfa
choi-hyeseong <bd2845@kakao.com>
2024-06-27 01:28:15 +0900

    test: fix test for packet payload

--- common/src/test/java/com/kiki/common/packet/PacketTest.kt
+++ common/src/test/java/com/kiki/common/packet/PacketTest.kt
@@ -1,6 +1,8 @@
 package com.kiki.common.packet
 
 import com.kiki.common.packet.type.PacketType
+import com.kiki.common.result.PayloadResult
+import com.kiki.common.result.exception.PayloadException
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.util.concurrent.TimeoutException
@@ -11,17 +13,17 @@ class PacketTest {
     @Test
     fun testAcceptPacket() {
         //accept로 된 패킷 테스트
-        val packet = Packet(1, PacketType.ACCEPT, Result.success(""))
+        val packet = Packet(1, PacketType.ACCEPT, PayloadResult.success(""))
         assertEquals(1, packet.id)
         assertEquals(PacketType.ACCEPT, packet.packetType)
-        assertTrue(packet.payload.isSuccess)
+        assertTrue(packet.payload.isSuccess())
     }
 
     @Test
     fun testPacketException() {
         //exception 전달 테스트
-        val packet = Packet(1, PacketType.MESSAGE, Result.failure(TimeoutException("타임아웃")))
-        assertTrue(packet.payload.isFailure)
+        val packet = Packet(1, PacketType.MESSAGE, PayloadResult.failure(PayloadException("타임아웃")))
+        assertFalse(packet.payload.isSuccess())
         val exception = packet.payload.exceptionOrNull()
         assertNotNull(exception)
         assertEquals("타임아웃", exception?.message)
