c883565563a4f19d9431baf3cb2c36dee76d0bd3
choi-hyeseong <bd2845@kakao.com>
2024-06-28 13:57:56 +0900

    fix: remove unnecessary thread on client 굳이 쓰레드풀 내에서 동작하는데, readSocket전용 쓰레드가 필요한가..?

--- common/src/main/kotlin/com/kiki/common/socket/ClientSocket.kt
+++ common/src/main/kotlin/com/kiki/common/socket/ClientSocket.kt
@@ -15,39 +15,34 @@ class ClientSocket(id : Int, outBoundQueue : PacketQueue, socket : Socket) : Abs
 
     @Volatile
     private var isRunning: Boolean = false
-    private lateinit var thread: Thread
 
     override fun run() {
         isRunning = true
-        thread = readSocket() //내부 쓰레드
+        readSocket()
     }
 
 
     // 연결된 소켓에서 읽어서 - 서버로 전달
-    private fun readSocket() : Thread {
-        val runnable = Runnable {
-            try {
-                val array = ByteArray(10000)
-                val reader = socket.getInputStream()
-                var readByte = reader.read(array)
-                while (readByte != -1 && isRunning) {
-                    val readPacket = array.slice(IntRange(0, readByte - 1)).toByteArray()
-                    sendPacket(Packet(id, PacketType.MESSAGE, readPacket))
-                    readByte = reader.read(array)
-                }
-            }
-            catch (e: Exception) {
-                println("Read error - $id | ${e.message}")
-                socket.close()
-                sendPacket(Packet(id, PacketType.MESSAGE, PayloadResult.failure(PayloadException("Connection reset")))) //에러 핸들
+    private fun readSocket() {
+        try {
+            val array = ByteArray(10000)
+            val reader = socket.getInputStream()
+            var readByte = reader.read(array)
+            while (readByte != -1 && isRunning) {
+                val readPacket = array.slice(IntRange(0, readByte - 1)).toByteArray()
+                sendPacket(Packet(id, PacketType.MESSAGE, readPacket))
+                readByte = reader.read(array)
             }
         }
-        return Thread.startVirtualThread(runnable) //가상 쓰레드로 시작
+        catch (e: Exception) {
+            println("Read error - $id | ${e.message}")
+            socket.close()
+            sendPacket(Packet(id, PacketType.MESSAGE, PayloadResult.failure(PayloadException("Connection reset")))) //에러 핸들
+        }
     }
 
     //소켓 작동 중단
     fun stopSocket() {
         isRunning = false
-        thread.interrupt() //쓰레드 중단
     }
 }
